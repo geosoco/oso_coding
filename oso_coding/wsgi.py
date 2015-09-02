7be5831d9e7275e101bfb82786dd4ae5daca9a71
John Robinson <soco@uw.edu>
2015-09-02 03:49:02 -0700

    update wsgi file with proper functionality

--- oso_coding/wsgi.py
+++ oso_coding/wsgi.py
@@ -4,13 +4,19 @@ WSGI config for oso_coding project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+def append_path(path):
+	if path not in sys.path:
+		sys.path.insert(0, path)
+
+append_path('/Users/soco/dev/projects/oso_coding')
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oso_coding.settings.local")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oso_coding.settings")
-
 application = get_wsgi_application()
